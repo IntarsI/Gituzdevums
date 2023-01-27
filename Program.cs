2b03fe66f5ff317a61ba19714829164647ab59d7
Intars Ilgsalietis <ii2762@gymn.local>
2023-01-27 08:47:39 +0200

    Pirma projekta augsupielade

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IstaisGituzdevums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
