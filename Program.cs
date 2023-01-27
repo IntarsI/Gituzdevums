c8e8a879305c67b0def7b9065b1185d938b55e63
Intars Ilgsalietis <ii2762@gymn.local>
2023-01-27 09:09:17 +0200

    Izveidota ievade

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace IstaisGituzdevums
         static void Main(string[] args)
         {
             int a = 0;
-            int b = 0;
+            int b = 1;
             int[,] arrd;
             a = Convert.ToInt32(Console.ReadLine());
             b = Convert.ToInt32(Console.ReadLine());
